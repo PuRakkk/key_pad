85ec7e4d586ab5c59490e9fb2b3ac426a95fde7a
PuRakkk <sarakchon11@gmail.com>
2025-03-18 17:50:16 +0700

    Test Vue

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -8,6 +8,6 @@ const router = useRouter();
 
 <template>
   <div class="h-screen flex items-center justify-center bg-slate-50">
-    <input class="bg-red-100 text-center" type="password">
+    <input class="w-10/12 p-2 text-center text-lg border-transparent rounded-md mb-3 bg-gray-200" v-model="pin" placeholder="Enter Admin Pin" maxlength="10" oninput="this.value = this.value.replace(/[^0-9]/g, '');" inputmode="none" autofocus type="password">
   </div>
 </template>
