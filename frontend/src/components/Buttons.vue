ab42ad10eebc6eb378547f143cfffcc24f06b1c3
PuRakkk <sarakchon11@gmail.com>
2025-03-19 16:50:57 +0700

    Keyboard

---
+++ frontend/src/components/Buttons.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["input", "delete", "next"]);
+</script>
+
+<template>
+    <div class="grid grid-cols-3 gap-4 w-10/12 mt-4">
+        <button
+            class="text-2xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-xl shadow-lg active:bg-white active:scale-10"
+            @click="emit('input', '7')">
+            7
+        </button>
+        <button
+            class="text-2xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl active:bg-white active:scale-10"
+            @click="emit('input', '8')">
+            8
+        </button>
+        <button
+            class="text-2xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl active:bg-white active:scale-10"
+            @click="emit('input', '9')">
+            9
+        </button>
+        <button
+            class="text-2xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl active:bg-white active:scale-10"
+            @click="emit('input', '4')">
+            4
+        </button>
+        <button
+            class="text-2xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl active:bg-white active:scale-10"
+            @click="emit('input', '5')">
+            5
+        </button>
+        <button
+            class="text-2xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl active:bg-white active:scale-10"
+            @click="emit('input', '6')">
+            6
+        </button>
+        <button
+            class="text-2xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl active:bg-white active:scale-10"
+            @click="emit('input', '1')">
+            1
+        </button>
+        <button
+            class="text-2xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl active:bg-white active:scale-10"
+            @click="emit('input', '2')">
+            2
+        </button>
+        <button
+            class="text-2xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl active:bg-white active:scale-10"
+            @click="emit('input', '3')">
+            3
+        </button>
+        <button
+            class="text-2xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl flex items-center justify-center active:bg-white active:scale-10"
+            @click="emit('delete')">
+            <i class="fas fa-backspace"></i>
+        </button>
+        <button
+            class="text-xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl active:bg-white active:scale-10"
+            @click="emit('input', '0')">
+            0
+        </button>
+        <button
+            class="text-xl font-bold border-gray-300 border bg-gray-100 w-18 h-16 rounded-lg shadow-xl flex items-center justify-center active:bg-white active:scale-10"
+            @click="emit('next')">
+            <i class="fas fa-angle-double-right"></i>
+        </button>
+    </div>
+</template>
---
+++ frontend/src/components/LoginPinInput.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import Buttons from "@/components/Buttons.vue";
+import router from "@/router";
+
+const pin = ref("");
+
+const handleInput = (value: string) => {
+    if (pin.value.length < 10) {
+        pin.value += value;
+    }
+};
+
+const handleDelete = () => {
+    pin.value = pin.value.slice(0, -10);
+};
+
+const handleNext = () => {
+    router.push("/home")
+};
+
+</script>
+
+<template>
+    <input 
+    class="w-10/12 h-12 p-2 font-bold text-center text-2xl border-gray-300 border rounded-md mb-3 bg-gray-50 shadow-xl placeholder-gray-400"
+    v-model="pin"
+    placeholder="Enter Admin Pin"
+    maxlength="10"
+    oninput="this.value = this.value.replace(/[^0-9]/g, '');"
+    type="password"
+    inputmode="none">
+
+    <Buttons 
+        @input="handleInput" 
+        @delete="handleDelete" 
+        @next="handleNext" 
+    />
+</template>
