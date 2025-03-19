8d748f47b8164e3519c77b0c0eeb72a73414f58e
PuRakkk <sarakchon11@gmail.com>
2025-03-19 11:33:45 +0700

    Laptop

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -3,11 +3,23 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+const pin = ref("");
 
 </script>
 
 <template>
   <div class="h-screen flex items-center justify-center bg-slate-50">
-    <input class="w-10/12 p-2 text-center text-lg border-transparent rounded-md mb-3 bg-gray-200" v-model="pin" placeholder="Enter Admin Pin" maxlength="10" oninput="this.value = this.value.replace(/[^0-9]/g, '');" inputmode="none" autofocus type="password">
+    <div class="w-[768px] flex flex-col items-center">
+      <input
+        class="w-10/12 p-2 text-center text-lg border-transparent rounded-md mb-3 bg-gray-200"
+        v-model="pin"
+        placeholder="Enter Admin Pin"
+        maxlength="10"
+        oninput="this.value = this.value.replace(/[^0-9]/g, '');"
+        inputmode="none"
+        autofocus
+        type="password"
+      />
+    </div>
   </div>
 </template>
